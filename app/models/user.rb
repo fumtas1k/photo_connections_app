628de645dbc221806268d9d50017795860f30497
fumtas1k <fumta01a7@gmail.com>
2022-01-18 12:00:39 +0900

    add:usersテーブルとモデルを作成（バリデーション込み）

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :full_name, presence: true, length: {maximum: 30}
+  validates :name, presence: true, length: {maximum: 20}
+  validates :email, presence: true, length: {maximum: 255}
+    format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w-]+\.[\w-]+\z/i},
+    uniqueness: true
+  before_validation {email.downcase!}
+  has_secure_password
+  validates :password, length: {minimum: 6}
+end

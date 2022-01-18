9214c8fd8ff9248f9959bff51899f27b74e980d4
fumtas1k <fumta01a7@gmail.com>
2022-01-18 11:39:26 +0900

    add:rails gの自動生成ファイルを制御

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,11 @@ module PhotoConnectionsApp
     config.load_defaults 6.0
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    donfig.generators do |g|
+      g.assets false
+      g.test_framework false
+      g.skip_routes true
+    end
   end
 end

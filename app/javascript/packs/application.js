e1680398492881bbbb8bc1e90d24f3b98fda9c99
fumtas1k <fumta01a7@gmail.com>
2022-01-21 09:31:25 +0900

    add:user formで必要項目入力までsubmit-btnをinactiveにする機能追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,4 +20,28 @@ $(document).on('turbolinks:load', function() {
     $("#show-mini-menu-wrapper").slideToggle(100);
     e.stopPropagation();
   });
+
+  function judge_btn_active(check_class){
+    let flag = true;
+    $(check_class).each(function(index){
+      if ($(check_class).eq(index).val() == ""){
+        flag = false;
+        return false;
+      }
+    });
+    return flag;
+  }
+  function set_btn_state(target_id, check_class){
+    if (judge_btn_active(check_class)){
+      $(target_id).prop("disabled", false);
+    }else{
+      $(target_id).prop("disabled", true);
+    }
+  }
+
+  set_btn_state("#active-btn", ".judge-active");
+  $(".judge-active").on("keyup", function(){
+    set_btn_state("#active-btn", ".judge-active");
+  });
+
 });

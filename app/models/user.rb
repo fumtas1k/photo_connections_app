4cc086ed731d03e1f00551df3c5d1c8711faf0ac
fumtas1k <fumta01a7@gmail.com>
2022-01-21 12:58:23 +0900

    fix:user emailのバリデーションを改修

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :full_name, presence: true, length: {maximum: 30}
   validates :name, presence: true, length: {maximum: 20}, uniqueness: true
   validates :email, presence: true, length: {maximum: 255},
-    format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w-]+\.[\w-]+\z/i},
+    format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w.-]+\.[\w-]+\z/i},
     uniqueness: true
   before_validation {email.downcase!}
   has_secure_password

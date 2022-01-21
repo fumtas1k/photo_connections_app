872efa8154e2ef2a9d632e0dc7dfaf4913917bb9
fumtas1k <fumta01a7@gmail.com>
2022-01-21 15:47:02 +0900

    fix:シードデータ生成時に、重複エラーが起こらないよう設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-
-50.times do
+n = 50
+names = Array.new(n) { Faker::Games::Pokemon.unique.name }
+emails = Array.new(n) { Faker::Internet.unique.email }
+n.times do |i|
   full_name = Faker::Name.name
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
+  name = names[i]
+  email = emails[i]
   password = "password"
   image = File.open("./app/assets/images/dummy_user.jpg")
   User.create!(full_name: full_name, name: name, email: email, password: password, image: image)

c024e3e855da90e4d810541a96cb93661b6514c7
fumtas1k <fumta01a7@gmail.com>
2022-01-21 12:05:15 +0900

    improve:userのnameが重複して登録されないよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :full_name, presence: true, length: {maximum: 30}
-  validates :name, presence: true, length: {maximum: 20}
+  validates :name, presence: true, length: {maximum: 20}, uniqueness: true
   validates :email, presence: true, length: {maximum: 255},
     format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w-]+\.[\w-]+\z/i},
     uniqueness: true
